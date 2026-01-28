c693450f99fa59cd62ac3fad82c2929a5cef1f31
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2026-01-28 22:05:18 +0900

    set content type

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,5 +1,8 @@
 export const onRequestGet = async (c) => {
   return new Response((await c.env.ASSETS.fetch(c.request)).body, {
     status: 410,
+    headers: {
+      'content-type': 'text/html; charset=utf-8',
+    },
   });
 };
